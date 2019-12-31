939d1ef166a29d81f5a61956535ec57b5901381f
Ricky chang <ricky50189@gmail.com>
2019-12-31 08:48:49 +0000

    update DSQLinear

--- DSQLinear.py
+++ DSQLinear.py
@@ -3,10 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+
 class RoundWithGradient(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x):
-        return x.round()
+        delta = torch.max(x) - torch.min(x)
+        x = (x/delta + 0.5)
+        return x.round() * 2 - 1
     @staticmethod
     def backward(ctx, g):
         return g 
@@ -18,56 +21,75 @@ class DSQLinear(nn.Linear):
         self.num_bit = num_bit
         self.quan_input = QInput
         self.bit_range = 2**self.num_bit -1	 
-        self.is_quan = bSetQ
+        self.is_quan = bSetQ        
+        self.momentum = momentum
         if self.is_quan:
             # using int32 max/min as init and backprogation to optimization
             # Weight
             self.uW = nn.Parameter(data = torch.tensor(2 **31 - 1).float())
             self.lW  = nn.Parameter(data = torch.tensor((-1) * (2**32)).float())
+            self.register_buffer('running_uw', torch.tensor([self.uW.data])) # init with uw
+            self.register_buffer('running_lw', torch.tensor([self.lW.data])) # init with lw
             self.alphaW = nn.Parameter(data = torch.tensor(0.2).float())
             # Bias
             if self.bias is not None:
                 self.uB = nn.Parameter(data = torch.tensor(2 **31 - 1).float())
                 self.lB  = nn.Parameter(data = torch.tensor((-1) * (2**32)).float())
+                self.register_buffer('running_uB', torch.tensor([self.uB.data]))# init with ub
+                self.register_buffer('running_lB', torch.tensor([self.lB.data]))# init with lb
                 self.alphaB = nn.Parameter(data = torch.tensor(0.2).float())
+                
 
             # Activation input		
             if self.quan_input:
                 self.uA = nn.Parameter(data = torch.tensor(2 **31 - 1).float())
                 self.lA  = nn.Parameter(data = torch.tensor((-1) * (2**32)).float())
+                self.register_buffer('running_uA', torch.tensor([self.uA.data])) # init with uA
+                self.register_buffer('running_lA', torch.tensor([self.lA.data])) # init with lA
                 self.alphaA = nn.Parameter(data = torch.tensor(0.2).float())
 
-    def phi_function(self, x, mi, alpha, delta):            
+    def clipping(self, x, upper, lower):
+        # clip lower
+        x = x + F.relu(lower - x)
+        # clip upper
+        x = x - F.relu(x - upper)
+
+        return x
+
+    def phi_function(self, x, mi, alpha, delta):
+
         # alpha should less than 2 or log will be None
-        alpha = alpha.clamp(None, 2)
+        # alpha = alpha.clamp(None, 2)
+        alpha = torch.where(alpha >= 2.0, torch.tensor([2.0]).cuda(), alpha)
         s = 1/(1-alpha)
-        k = (1/delta) * (2/alpha - 1).log()
-
-        out = s * ((k * (x - mi)).tanh())
-
-        return out  
+        k = (2/alpha - 1).log() * (1/delta)
+        x = (((x - mi) *k ).tanh()) * s 
+        return x	
 
     def sgn(self, x):
-        # x = torch.where(x>=0, 1.0, -1.0)
-        # where does support autograd
-        # use normolize and round instead
-        delta = torch.max(x) - torch.min(x)
-        x = ((x - torch.min(x))/delta)
-        x = RoundWithGradient.apply(x) * 2 -1
+        x = RoundWithGradient.apply(x)
 
         return x
 
     def dequantize(self, x, lower_bound, delta, interval):
 
-        out = lower_bound + delta * (interval + (x+1)/2)
+        # save mem
+        x =  ((x+1)/2 + interval) * delta + lower_bound
 
-        return out
+        return x
 
     def forward(self, x):
         if self.is_quan:
             # Weight Part
-            Qweight = torch.where(self.weight >= self.uW, self.uW, self.weight)
-            Qweight = torch.where(Qweight <= self.lW, self.lW, Qweight)
+            # moving average
+            if self.training:
+                cur_running_lw = self.running_lw.mul(1-self.momentum).add((self.momentum) * self.lW)
+                cur_running_uw = self.running_uw.mul(1-self.momentum).add((self.momentum) * self.uW)
+            else:
+                cur_running_lw = self.running_lw
+                cur_running_uw = self.running_uw
+
+            Qweight = self.clipping(self.weight, cur_running_uw, cur_running_lw)
             cur_max = torch.max(Qweight)
             cur_min = torch.min(Qweight)
             delta =  (cur_max - cur_min)/(self.bit_range)
@@ -77,11 +99,19 @@ class DSQLinear(nn.Linear):
             Qweight = self.sgn(Qweight)
             Qweight = self.dequantize(Qweight, cur_min, delta, interval)
 
-            Qbias = None
+            Qbias = self.bias
             # Bias			
             if self.bias is not None:
-                Qbias = torch.where(self.bias >= self.ub, self.ub, self.bias)
-                Qbias = torch.where(Qbias <= self.lb, self.lb, Qbias)
+                # self.running_lB.mul_(1-self.momentum).add_((self.momentum) * self.lB)
+                # self.running_uB.mul_(1-self.momentum).add_((self.momentum) * self.uB)
+                if self.training:
+                    cur_running_lB = self.running_lB.mul(1-self.momentum).add((self.momentum) * self.lB)
+                    cur_running_uB = self.running_uB.mul(1-self.momentum).add((self.momentum) * self.uB)
+                else:
+                    cur_running_lB = self.running_lB
+                    cur_running_uB = self.running_uB
+
+                Qbias = self.clipping(self.bias, cur_running_uB, cur_running_lB)
                 cur_max = torch.max(Qbias)
                 cur_min = torch.min(Qbias)
                 delta =  (cur_max - cur_min)/(self.bit_range)
@@ -93,14 +123,21 @@ class DSQLinear(nn.Linear):
 
             # Input(Activation)
             Qactivation = x
-            if self.quan_input:                
-                Qactivation = torch.where(x >= self.uA, self.uA, x)
-                Qactivation = torch.where(Qactivation <= self.lA, self.lA, Qactivation)
+            if self.quan_input:
+                                
+                if self.training:                    
+                    cur_running_lA = self.running_lA.mul(1-self.momentum).add((self.momentum) * self.lA)
+                    cur_running_uA = self.running_uA.mul(1-self.momentum).add((self.momentum) * self.uA)
+                else:
+                    cur_running_lA = self.running_lA
+                    cur_running_uA = self.running_uA
+                    
+                Qactivation = self.clipping(x, cur_running_uA, cur_running_lA)
                 cur_max = torch.max(Qactivation)
                 cur_min = torch.min(Qactivation)
                 delta =  (cur_max - cur_min)/(self.bit_range)
                 interval = (Qactivation - cur_min) //delta
-                mi = (interval + 0.5) * delta + cur_min
+                mi = (interval + 0.5) * delta + cur_min                
                 Qactivation = self.phi_function(Qactivation, mi, self.alphaA, delta)
                 Qactivation = self.sgn(Qactivation)
                 Qactivation = self.dequantize(Qactivation, cur_min, delta, interval)
